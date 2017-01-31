44fd06d51d8e1c903b0962b3ced8629253d93419
atkallie <kal17da@gmail.com>
2017-01-31 13:35:53 -0500

    Initial Commit

--- TableViews/ViewController.swift
+++ TableViews/ViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //create content to populate table
+    let tableContent : [String]=["A","B","C","D","E"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return tableContent.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        //first you have to create a UITableViewCell object and base it off of the default style of the prototype cell
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        //set value of cell's content
+        
+        cell.textLabel?.text = tableContent[indexPath.row]
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
